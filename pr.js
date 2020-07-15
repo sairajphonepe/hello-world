cb50befe79482f5205a9ff332d6fe97255fa55b4
DevendraDev <devendradeotale@yahoo.com>
2020-07-15 06:16:27 +0530

    Update pr.js

--- pr.js
+++ pr.js
@@ -8,10 +8,17 @@ function buildPaymentRequest(url) {
       return null;
     }
     
+//     const supportedInstruments = [{
+//         supportedMethods: "https://phonepay.herokuapp.com/pay",
+//         data: {
+//             url: url
+//         }
+//     }];
+    
     const supportedInstruments = [{
-        supportedMethods: "https://phonepay.herokuapp.com/pay",
+        supportedMethods: "https://mercury.phonepe.com/transact/pay",
         data: {
-            url: url
+            url: "upi://pay?pa=PRACT0@ybl&pn=PRACT0&am=1.0&mam=1.0&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00"
         }
     }];
   
