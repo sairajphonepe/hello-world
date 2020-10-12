6a6ac92f637ddd8c950655358fcb3421df702f03
DevendraDev <devendradeotale@yahoo.com>
2020-10-12 16:27:20 +0530

    Update pr.js

--- pr.js
+++ pr.js
@@ -15,12 +15,25 @@ function buildPaymentRequest(url) {
 //         }
 //     }];
     
-    const supportedInstruments = [{
-        supportedMethods: "https://tez.google.com/pay",
-        data: {
-            url: "upi://pay?pa=PRACT0@ybl&pn=PRACT0&am=1.0&mam=1.0&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00"
-        }
+    const supportedInstruments = [
+     {
+      supportedMethods: ['https://tez.google.com/pay'],
+      data: {
+        pa: 'PRACT0@ybl',
+        pn: 'PRACT',
+        tr: 'T2002061921587731419308',  // your custom transaction reference ID
+        url: 'upi://pay?pa=PRACT0@ybl&pn=PRACT0&am=1.0&mam=1.0&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00',
+        mc: '5311', // your merchant category code
+        tn: 'Payment',
+      },
     }];
+    
+//     const supportedInstruments = [{
+//         supportedMethods: "https://tez.google.com/pay",
+//         data: {
+//             url: "upi://pay?pa=PRACT0@ybl&pn=PRACT0&am=1.0&mam=1.0&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00"
+//         }
+//     }];
   
     const details = {
       total: {
