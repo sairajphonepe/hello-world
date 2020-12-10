c33869b66bafe1574b4104582c9e4933837d40f5
DevendraDev <devendradeotale@yahoo.com>
2020-12-10 14:12:07 +0530

    Update prWithOptions.js

--- prWithOptions.js
+++ prWithOptions.js
@@ -1,88 +1,11 @@
-let paymentRequestTimeout, paymentRequest, timeOutCounter= 0;
-function reset(){
-  document.getElementById('msg').innerHTML = ""; 
-  if(document.getElementById("other").checked == true) {
-    if(document.getElementById("otherPaymentAppPaymentUrl").value == "" || document.getElementById("otherPaymentAppPaymentUrl").value == " "){
-      alert("Please enter the supportedMethod to create Payment Request object");
-    } else {
-      createPaymentRequest(false, document.getElementById("otherPaymentAppPaymentUrl").value);
-    }
-    return;
-  }
-  if(document.getElementById("phonepe").checked == true) {
-    createPaymentRequest(true, "phonepe");
-    return;
-  }
-  if(document.getElementById("gpay").checked == true) {
-    createPaymentRequest(true, "gpay");
-    return;
-  }
-}
-
-function changeHandlerOther(evt) {
-  if(document.getElementById("phonepe").checked == true) {
-    document.getElementById("otherPaymentAppInput").className = "hiddenElement";
-  }
-  if(document.getElementById("gpay").checked == true) {
-    document.getElementById("otherPaymentAppInput").className = "hiddenElement";
-//     createPaymentRequest(true, "gpay");
-  }
-  if(document.getElementById("other").checked == true) {
-    document.getElementById("otherPaymentAppInput").className = "";
-  }
-  reset();
-}
-
-function onPageLoad() {
-  reset();
-}
-
-function onProceedSelectedAppHasEnrolledInstrument(evt) {
-   paymentRequest && paymentRequest.hasEnrolledInstrument().then(function(result) {
-          info("here hasEnrolledInstrument result= " + result); 
-      }).catch(function(err) {
-          info("here hasEnrolledInstrument error handler and error= " + err); 
-      });
-  console.log("We are here in onProceedSelectedApp hasEnrolledInstrument", evt);
-}
-
-function onProceedSelectedAppCanMakePayment(evt) {
-   paymentRequest && paymentRequest.canMakePayment().then(function(result) {
-          info("here canMakePayment result= " + result); 
-      }).catch(function(err) {
-          info("here canMakePayment error handler and error= " + err); 
-      });
-  console.log("We are here in onProceedSelectedApp canMakePayment", evt);
-}
-
-function createPaymentRequest(bDirectApp, sAppUrl){
-  if (!window.PaymentRequest) {
-      // paymentRequest not supported for this browser.
-      info("Here paymentRequest not supported");
-      return;
-    }
-  const transactionDetails = {
-      total: {
-        label: 'Total',
-        amount: {
-          currency: 'INR',
-          value: '100',
-        }
-      }
-    };
-  var supportedInstruments;
-  
-  if(bDirectApp) {
-    if(sAppUrl == "phonepe"){
-      supportedInstruments = [{
+let paymentRequestTimeout, paymentRequestPhonepe, paymentRequestGPay, paymentRequestOther, timeOutCounter= 0;
+const supportedInstrumentsPhonepe = [{
           supportedMethods: ["https://mercury.phonepe.com/transact/pay"],
           data: {
               url: "upi://pay?pa=PRACT0@ybl&pn=PRACT0&am=1.0&mam=1.0&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00"
           }
-      }];
-    }
-    if (sAppUrl == "gpay"){
-      supportedInstruments = [{
+      }],
+      supportedInstrumentGPay = [{
           supportedMethods: ["https://tez.google.com/pay"],
           data: {
              pa: 'PRACT0@ybl',
@@ -92,17 +15,86 @@ function createPaymentRequest(bDirectApp, sAppUrl){
              mc: '5311', // your merchant category code
              tn: 'Payment',
            },
-         }];
-          }
-  } else {
-    supportedInstruments = [{
-          supportedMethods: [sAppUrl],
+         }],
+      supportedInstrumnetOthers = [{
+          supportedMethods: ["temp"],
           data: {
             url: "upi://pay?pa=PRACT0@ybl&pn=PRACT0&am=1.0&mam=1.0&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00"
           }
-      }];
+      }],
+      transactionDetails = {
+      total: {
+        label: 'Total',
+        amount: {
+          currency: 'INR',
+          value: '100',
+        }
+      }
+    };
+
+function reset(){
+  document.getElementById('msg').innerHTML = ""; 
+  createPaymentRequest();
+}
+
+function getSelctedApp() {
+  if(document.getElementById("phonepe").checked == true) {
+    return "phonepe";
+  } else {
+    return "gpay";
   }
-  paymentRequest = new PaymentRequest(supportedInstruments, transactionDetails);
+}
+
+function onPageLoad() {
+  createPaymentRequest();
+}
+
+function onProceedSelectedAppHasEnrolledInstrument(evt) {
+  let selectedApp = getSelctedApp();
+  if (selectedApp == "phonepe") {
+    paymentRequestPhonepe && paymentRequestPhonepe.hasEnrolledInstrument().then(function(result) {
+          info("For Phonepe hasEnrolledInstrument result= " + result); 
+      }).catch(function(err) {
+          info("For Phonepe hasEnrolledInstrument error handler and error= " + err); 
+      });
+      } else {
+        paymentRequestGPay  && paymentRequestGPay.hasEnrolledInstrument().then(function(result) {
+          info("For GPay hasEnrolledInstrument result= " + result); 
+      }).catch(function(err) {
+          info("For GPay hasEnrolledInstrument error handler and error= " + err); 
+      });
+      }
+   
+  console.log("We are here in onProceedSelectedApp hasEnrolledInstrument", evt);
+}
+
+function onProceedSelectedAppCanMakePayment(evt) {
+  let selectedApp = getSelctedApp();
+  if (selectedApp == "phonepe") {
+   paymentRequestPhonepe && paymentRequestPhonepe.canMakePayment().then(function(result) {
+          info("For Phonpepe canMakePayment result= " + result); 
+      }).catch(function(err) {
+          info("For Phonpepe canMakePayment error handler and error= " + err); 
+      });
+  } else {
+    paymentRequestGPay  && paymentRequestGPay.canMakePayment().then(function(result) {
+          info("For GPay canMakePayment result= " + result); 
+      }).catch(function(err) {
+          info("For GPay canMakePayment error handler and error= " + err); 
+      });
+  }
+  console.log("We are here in onProceedSelectedApp canMakePayment", evt);
+}
+
+function createPaymentRequest(bDirectApp, sAppUrl){
+  if (!window.PaymentRequest) {
+      // paymentRequest not supported for this browser.
+      info("Here paymentRequest not supported");
+      return;
+    }
+  paymentRequestPhonepe = new PaymentRequest(supportedInstrumentsPhonepe, transactionDetails);
+  paymentRequestGPay  = new PaymentRequest(supportedInstrumentGPay, transactionDetails);
+//   paymentRequestOther  = new PaymentRequest(supportedInstrumnetOthers, transactionDetails);
 }
 
 function info(msg) {
@@ -123,13 +115,20 @@ function handlePaymentResponse(response) {
  }
 
 function onPayClick() { 
-    
-  
-  paymentRequest.show()
-                .then(handlePaymentResponse)
-                .catch(function(err) {
-                  info(err);
-                });
+  let selectedApp = getSelctedApp();
+    if (selectedApp == "phonepe") {
+          paymentRequestPhonepe.show()
+                        .then(handlePaymentResponse)
+                        .catch(function(err) {
+                          info(err);
+                        });
+    } else {
+      paymentRequestGPay.show()
+                        .then(handlePaymentResponse)
+                        .catch(function(err) {
+                          info(err);
+                        });
+    }
 
 
 }
