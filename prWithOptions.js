94e82ecba9f0b4d79d6ea08a3f3cd08b1312a2cd
DevendraDev <devendradeotale@yahoo.com>
2021-04-16 00:23:53 +0530

    Update prWithOptions.js

--- prWithOptions.js
+++ prWithOptions.js
@@ -1,4 +1,4 @@
-let paymentRequestTimeout, paymentRequestPhonepe, paymentRequestGPay, paymentRequestOther, timeOutCounter= 0;
+let paymentRequestTimeout, paymentRequestPhonepe, paymentRequestPhonepeStage, paymentRequestGPay, paymentRequestOther, timeOutCounter= 0;
 const supportedInstrumentsPhonepe = [{
           supportedMethods: ["https://mercury.phonepe.com/transact/pay"],
           data: {
@@ -40,7 +40,11 @@ function reset(){
 function getSelctedApp() {
   if(document.getElementById("phonepe").checked == true) {
     return "phonepe";
-  } else {
+  } 
+  if(document.getElementById("phonepeStage").checked == true) {
+    return "phonepestage";
+  }
+  if(document.getElementById("gpay").checked == true) {
     return "gpay";
   }
 }
@@ -57,14 +61,21 @@ function onProceedSelectedAppHasEnrolledInstrument(evt) {
       }).catch(function(err) {
           info("For Phonepe hasEnrolledInstrument error handler and error= " + err); 
       });
-      } else {
+      } 
+   if (selectedApp == "phonepestage") {
+        paymentRequestPhonepeStage  && paymentRequestPhonepeStage.hasEnrolledInstrument().then(function(result) {
+          info("For Phonepe stage hasEnrolledInstrument result= " + result); 
+      }).catch(function(err) {
+          info("For Phonepe stage hasEnrolledInstrument error handler and error= " + err); 
+      });
+      }
+   if (selectedApp == "gpay") {
         paymentRequestGPay  && paymentRequestGPay.hasEnrolledInstrument().then(function(result) {
           info("For GPay hasEnrolledInstrument result= " + result); 
       }).catch(function(err) {
           info("For GPay hasEnrolledInstrument error handler and error= " + err); 
       });
       }
-   
   console.log("We are here in onProceedSelectedApp hasEnrolledInstrument", evt);
 }
 
@@ -76,7 +87,15 @@ function onProceedSelectedAppCanMakePayment(evt) {
       }).catch(function(err) {
           info("For Phonpepe canMakePayment error handler and error= " + err); 
       });
-  } else {
+  } 
+  if (selectedApp == "phonepestage") {
+    paymentRequestPhonepeStage  && paymentRequestPhonepeStage.canMakePayment().then(function(result) {
+          info("For Phonepe stage canMakePayment result= " + result); 
+      }).catch(function(err) {
+          info("For Phonepe stage canMakePayment error handler and error= " + err); 
+      });
+  }
+  if (selectedApp == "gpay") {
     paymentRequestGPay  && paymentRequestGPay.canMakePayment().then(function(result) {
           info("For GPay canMakePayment result= " + result); 
       }).catch(function(err) {
@@ -94,6 +113,12 @@ function createPaymentRequest(bDirectApp, sAppUrl){
     }
   paymentRequestPhonepe = new PaymentRequest(supportedInstrumentsPhonepe, transactionDetails);
   paymentRequestGPay  = new PaymentRequest(supportedInstrumentGPay, transactionDetails);
+  paymentRequestPhonepeStage = new PaymentRequest([{
+          supportedMethods: ["https://mercury-stg.phonepe.com/transact/pay"],
+          data: {
+              url: "upi://pay?pa=PRACT0@ybl&pn=PRACT0&am=1.0&mam=1.0&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00"
+          }
+      }], transactionDetails);
 //   paymentRequestOther  = new PaymentRequest(supportedInstrumnetOthers, transactionDetails);
 }
 
@@ -122,7 +147,15 @@ function onPayClick() {
                         .catch(function(err) {
                           info(err);
                         });
-    } else {
+    }
+    if (selectedApp == "phonepestage") {
+      paymentRequestPhonepeStage.show()
+                        .then(handlePaymentResponse)
+                        .catch(function(err) {
+                          info(err);
+                        });
+    }
+    if (selectedApp == "gpay") {
       paymentRequestGPay.show()
                         .then(handlePaymentResponse)
                         .catch(function(err) {
