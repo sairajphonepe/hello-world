e83d073bda1200fc690b9e23ad9f2d623709d747
DevendraDev <devendradeotale@yahoo.com>
2021-08-17 12:48:17 +0530

    Update prWithOptions.js

--- prWithOptions.js
+++ prWithOptions.js
@@ -117,7 +117,8 @@ function createPaymentRequest(bDirectApp, sAppUrl){
   paymentRequestPhonepeStage = new PaymentRequest([{
           supportedMethods: ["https://mercury-stg.phonepe.com/transact/pay"],
           data: {
-              url: "upi://pay?pa=PRACT0@ybl&pn=PRACT0&am=1.0&mam=1.0&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00"
+              url: "upi://pay?pa=PRACT0@ybl&pn=PRACT0&am=1.0&mam=1.0&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00",
+                    mobileNumber: "9890908888"
           }
       }], transactionDetails);
 //   paymentRequestOther  = new PaymentRequest(supportedInstrumnetOthers, transactionDetails);
